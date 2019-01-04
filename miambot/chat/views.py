2e48e986aba9594661d5c46b2a859466f269e0fd
marom17 <romain.flac@gmail.com>
2019-01-04 14:05:53 +0100

    chat respond to test url

--- miambot/chat/views.py
+++ miambot/chat/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from miambot.urls import chatbot
 
 def index(request):
     return HttpResponse("Hello, world. You're at the chat index.")
+
+def bot(request):
+    resp = chatbot.respond('hello')
+    return HttpResponse(resp)
 
 
--- miambot/miambot/urls.py
+++ miambot/miambot/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from bot import MiamBot
+
+chatbot = MiamBot()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
