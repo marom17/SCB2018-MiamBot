0f99d5fc40138f5605ed5ad3ed8d109da60b9681
marom17 <romain.flac@gmail.com>
2019-01-09 17:44:09 +0100

    working searching in database

--- miambot/bot/processing.py
+++ miambot/bot/processing.py
@@ -18,9 +18,64 @@ class Processor():
         msgType = bot.getPredicate('type', chatId)
         print(msgType)
         if(msgType not in "answer"):
-            food = bot.getPredicate('food', chatId)
-            drink = bot.getPredicate('drink', chatId)
-            Search.search('apple', self.data, True, False)
-            print(food)
-            print(drink)
-            #request.post("https://api.telegram.org/bot"+token+"/sendMessage",data={'chat_id':chatId, 'text':'test', 'parse_mode':'HTML'})
+            if(msgType in ["food", "drink", "fooddrink"]):
+                food = bot.getPredicate('food', chatId).split(', ')
+                drink = bot.getPredicate('drink', chatId).split(', ')
+                matching = []
+                notFound = []
+                print(food)
+                print(drink)
+                if(msgType in "food"):
+                    for f in food:
+                        found = Search.search(f, self.data, True)
+                        if(len(found) == 0):
+                            notFound.append(f)
+                        else:
+                            matching.append(found)
+                elif(msgType in "drink"):
+                    for d in drink:
+                        found = Search.search(d, self.data, False)
+                        if(len(found) == 0):
+                            notFound.append(d)
+                        else:
+                            matching.append(found)
+                else:
+                    for f in food:
+                        found = Search.search(f, self.data, True)
+                        if(len(found) == 0):
+                            notFound.append(f)
+                        else:
+                            matching.append(found)
+                    for d in drink :
+                        found = Search.search(d, self.data, False)
+                        if(len(found) == 0):
+                            notFound.append(d)
+                        else:
+                            matching.append(found)
+                if(len(notFound) != 0):
+                    string = ""
+                    for nf in notFound:
+                        string+= nf+", "
+                    string = string[:-2]
+                    bot.setPredicate("notFoundList", string, chatId)
+                    resp = bot.respond("NOTFOUND", chatId)
+                    #request.post("https://api.telegram.org/bot"+token+"/sendMessage",data={'chat_id':chatId, 'text':resp, 'parse_mode':'HTML'})
+                else:
+                    resp = bot.respond("ALLFOUND", chatId)
+                    #request.post("https://api.telegram.org/bot"+token+"/sendMessage",data={'chat_id':chatId, 'text':resp, 'parse_mode':'HTML'})
+                
+            elif(msgType in "search"):
+                toSearch = bot.getPredicate('search', chatId)
+                matching = Search.search(toSearch, self.data, True)
+                matching += Search.search(toSearch, self.data, False)
+                if(len(matching) == 0):
+                    bot.setPredicate("notFoundList", toSearch, chatId)
+                    resp = bot.respond("NOTFOUND", chatId)
+                    #request.post("https://api.telegram.org/bot"+token+"/sendMessage",data={'chat_id':chatId, 'text':resp, 'parse_mode':'HTML'})
+                else:
+                    resp = "Found in database:\n"
+                    for f in matching:
+                        resp += "* "+f['name']+"\n"
+                    print(resp)
+                    #request.post("https://api.telegram.org/bot"+token+"/sendMessage",data={'chat_id':chatId, 'text':resp, 'parse_mode':'HTML'})
+            
--- miambot/bot/search.py
+++ miambot/bot/search.py
@@ -5,22 +5,16 @@ class Search():
     nofood = [0,4,5]
 
     @staticmethod
-    def search(value, data, food, searchFood):
+    def search(value, data, food):
         matching = []
-        if(searchFood):
-            toSearch = value
-        else:
-            toSearch ="^" + value
 
         for k in data['food-items']:
             if(food and not k['liquid']):
-                if(re.search(toSearch, k['name'], re.IGNORECASE)):
+                if(re.search(value, k['name'], re.IGNORECASE)):
                     matching.append(k)
-                    print(k['name'])
 
             elif(not food and k['liquid']):
-                if(re.search(toSearch, k['name'], re.IGNORECASE)):
+                if(re.search(value, k['name'], re.IGNORECASE)):
                     matching.append(k)
-                    print(k['name'])
-                    
+
         return matching
