62d5d207f716df8d91052a34833c5ff73d44eccf
rom <rom@fbpc3.epfl.ch>
2019-01-09 11:21:53 +0100

    correct code to be telegram compliant

--- miambot/chat/views.py
+++ miambot/chat/views.py
@@ -11,13 +11,13 @@ def index(request):
 @csrf_exempt
 def bot(request):
     if request.method == 'POST':
-        json_data = json.loads(request.body)
+        json_data = json.loads(request.body.decode('utf-8'))
         msg = json_data['message']['text']
         usrName = json_data['message']['from']['first_name']
         usrId = json_data['message']['from']['id']
         chatId = json_data['message']['chat']['id']
         resp = chatbot.interact(msg, usrName, usrId, chatId)
-        return HttpResponse('{"method":"sendMessage", "chat_id":'+str(chatId)+', "text":"'+resp+'"}', status=200, content_type="application/json")
+        return HttpResponse('{"method":"sendMessage", "chat_id":'+str(chatId)+', "text":"'+resp+'", "parse_mode":"HTML"}', status=200, content_type="application/json")
     return HttpResponse("Forbidden",status=403)
 
 
